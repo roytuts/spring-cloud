58405b92c5a8fb4fadcc4301fa47e26e5de516da
Soumitra Roy Sarkar <projwebinfo@gmail.com>
2020-04-28 18:53:45 +0530

    Add files via upload

---
+++ spring-cloud-gateway/src/main/java/com/roytuts/spring/cloud/gateway/SpringCloudGatewayApp.java
@@ -0,0 +1,13 @@
+package com.roytuts.spring.cloud.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudGatewayApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudGatewayApp.class, args);
+	}
+
+}
