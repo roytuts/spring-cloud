befae9b7a60b6de0fd83dd1f57cb1c78fa048a61
Soumitra <email@email.com>
2021-03-15 08:13:27 +0530

    spring cloud bus example

---
+++ spring-cloud-bus-apache-kafka/spring-dynamic-runtime-configurations-client2/src/main/java/com/roytuts/spring/dynamic/runtime/configurations/client/SpringDynamicRuntimeConfigClient2App.java
@@ -0,0 +1,13 @@
+package com.roytuts.spring.dynamic.runtime.configurations.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDynamicRuntimeConfigClient2App {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDynamicRuntimeConfigClient2App.class, args);
+	}
+
+}
